99d6957a4dd5b36ba433d999546c4b149aa62e14
i-shintaro <s.iwashita@di-v.co.jp>
2024-04-08 18:57:32 +0900

    nicknameが空の場合のexpectationを記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    describe 'ユーザー新規登録' do  #ユーザー新規登録についてのテストコードを記述
+      it 'nicknameが空では登録できない'do #nicknameが空では登録できなテストコードを記述
+        user = User.new(nickname: '', email: 'test@example',password:'000000',password_confirmation: '000000')
+        user.valid?
+        expect(user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it 'emailが空では登録できない' do #emailが空では登録できないテストコードを記述
+      end
+  end
 end
