e49e6334a7b30a7cc17a8cdbc97b9a40bf6fe098
i-shintaro <s.iwashita@di-v.co.jp>
2024-04-10 10:47:59 +0900

    Factorybot導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
     describe 'ユーザー新規登録' do  #ユーザー新規登録についてのテストコードを記述
       it 'nicknameが空では登録できない'do #nicknameが空では登録できなテストコードを記述
-        user = User.new(nickname: '', email: 'test@example',password:'000000',password_confirmation: '000000')
+        user = FactoryBot.build(:user) #Userインスタンス生成
+        user.nickname = '' #nicknemeを空にする
         user.valid?
         expect(user.errors.full_messages).to include("Nickname can't be blank")
       end
       it 'emailが空では登録できない' do #emailが空では登録できないテストコードを記述
-        user = User.new(nickname: 'test', email: '', password: '000000', password_confirmation: '000000')
+        user = FactoryBot.build(:user) #Userインスタンス生成
+        user.email = '' #nicknemeを空にする
         user.valid?
         expect(user.errors.full_messages).to include("Email can't be blank")
       end
