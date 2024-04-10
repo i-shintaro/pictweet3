e8a0056ebf5d7e8bf323d2bc8c55f4c834eeb8ad
i-shintaro <s.iwashita@di-v.co.jp>
2024-04-10 10:55:30 +0900

    beforeを使い共通した記述を切り出し

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
     describe 'ユーザー新規登録' do  #ユーザー新規登録についてのテストコードを記述
       it 'nicknameが空では登録できない'do #nicknameが空では登録できなテストコードを記述
-        user = FactoryBot.build(:user) #Userインスタンス生成
-        user.nickname = '' #nicknemeを空にする
-        user.valid?
-        expect(user.errors.full_messages).to include("Nickname can't be blank")
+        @user.nickname = '' #nicknemeを空にする
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
       it 'emailが空では登録できない' do #emailが空では登録できないテストコードを記述
-        user = FactoryBot.build(:user) #Userインスタンス生成
-        user.email = '' #nicknemeを空にする
-        user.valid?
-        expect(user.errors.full_messages).to include("Email can't be blank")
+        @user.email = '' #nicknemeを空にする
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
       end
   end
 end
